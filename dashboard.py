ee688397cb91fa379b0d21c9025ebe8be686c02d
Chris Lutze <clutze@gmail.com>
2023-07-12 21:54:56 -1000

    buoy search implemented

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import requests
 import urllib.parse
 from geopy.geocoders import Nominatim
+import xml.etree.ElementTree as ET
 from tkinter import *
 from tkinter import ttk
 
@@ -20,6 +21,47 @@ def location_search():
     except Exception as e:
         print(e)
         return None, None
+
+
+def buoy_search():
+    print("Buoy Search")
+    # parse active station list
+    #loadStations()
+    parsed_stations = parseXML('activestations.xml')
+    result = []
+    latitude_num = float(latitude.get())
+    longitude_num = float(longitude.get())
+    radius_num = float(search_radius.get())
+    for station in parsed_stations:
+        station_lat = float(station.get('lat'))
+        station_lon = float(station.get('lon'))
+        if (latitude_num - radius_num) <= station_lat <= (latitude_num + radius_num):
+            if (longitude_num - radius_num) <= station_lon <= (longitude_num + radius_num):
+                result.append(station)
+    #print(result)
+    searched_buoys.set(result)
+    #thing = searched_buoys.get()
+
+
+
+def loadStations():
+    station_list_url = "http://www.ndbc.noaa.gov/activestations.xml"
+    response = requests.get(station_list_url)
+    with open('activestations.xml', 'wb') as f:
+        f.write(response.content)
+
+
+def parseXML(xmlfile):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    station_list = []
+    for child in root:
+        station = child.attrib
+        station_list.append(station)
+    return station_list
+
+
+#buoy_search()
 
 win = Tk()      # Instance of Tkinter frame
 win.title("Buoy Data Dashboard")
@@ -41,17 +83,17 @@ location_field.grid(column=1, row=1, columnspan=2, sticky=(W, E))
 # Create Button for location search
 ttk.Button(mainframe, text="Search",width=15, command=location_search).grid(column=3, row=1)
 # Create Display label for location search result
-latitude_label = Label(mainframe, text="Latitude")
-latitude_label.grid(column=0, row=2)
+latitude_label = Label(mainframe, text="Latitude").grid(column=0, row=2)
 latitude = StringVar(mainframe)
-latitude_field = Entry(mainframe, textvariable=latitude)
-latitude_field.grid(column=1, row=2)
-longitude_label = Label(mainframe, text="Longitude")
-longitude_label.grid(column=2, row=2)
+latitude_field = Entry(mainframe, textvariable=latitude).grid(column=1, row=2)
+longitude_label = Label(mainframe, text="Longitude").grid(column=2, row=2)
 longitude = StringVar(mainframe)
-longitude_field = Entry(mainframe, textvariable=longitude)
-longitude_field.grid(column=3, row=2)
-
+longitude_field = Entry(mainframe, textvariable=longitude).grid(column=3, row=2)
+radius_label = Label(mainframe, text="Radius").grid(column=4, row=2)
+search_radius = StringVar(mainframe)
+radius_field = Entry(mainframe, textvariable=search_radius).grid(column=5, row=2)
+ttk.Button(mainframe, text="Search", width=15, command=buoy_search).grid(column=6, row=2)
+searched_buoys = Variable()
 
 win.mainloop()
 
